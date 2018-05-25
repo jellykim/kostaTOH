cc627ce21bb9eccce4ea927ee08a7ca83d6c3125
jellykim <bbffvxd@naver.com>
2018-05-25 13:27:28 +0900

    tour of hero app Second Commit

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,16 +3,19 @@ import {Hero} from './hero';
 import {HEROES} from './mockHeros';
 import {Observable, of} from 'rxjs';
 import {MessageService} from './message.service';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesURL = 'http://localhost:8087/heroes';
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of (HEROES);
+    // return of (HEROES);
+    return this.http.get<Hero[]>(this.heroesURL);
   }
 }
